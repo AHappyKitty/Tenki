f6983ad12f4aa61a32dd56f9951736499f2ef426
AHappyKitty <mingda.yang@hyperionlyceum.nl>
2016-06-09 21:10:17 +0200

    Update app.js

--- WeatherAppStarter/app.js
+++ WeatherAppStarter/app.js
@@ -145,28 +145,31 @@ function chooseGreet(weather) {
 	if (currentHour >= 6 && currentHour < 12) {
 		return morningGreet();
 	} else if (currentHour >= 12 && currentHour < 18) {
-		return afternoonGreet;
+		return afternoonGreet();
 	} else if (currentHour >= 18 && currentHour <= 23) {
-		return eveningGreet;
+		return eveningGreet();
 	} else if (currentHour > 0 && currentHour < 6) {
-		return nightGreet;
+		return nightGreet();
 	}
 	
 	function morningGreet() {
-		var morningChoices = ["Goede morgen!", "Lekker geslapen?", "Een start van een nieuwe dag!"];
+		var morningChoices = ["Good morning!", "Hello sleepy head!", "Another day, another morning! Ready yet?", "Well isn't it fun? The start of the new day!", "How was breakfast? Or are you up this early?", "Well it's a beatutiful morning, isn't it?", "Quick, drink some coffee before you fall back to sleep!", "Going out early today?"];
 		return morningChoices[Math.floor(Math.random()*morningChoices.length)];
 	}
 	
 	function afternoonGreet() {
-		
+		var afternoonChoices = ["Good afternoon!", "Eat a snack, helps you get through the day.", "Keep up the good work! And you'll be eating diner in no time.", "Don't snack too much now, otherwise you'll get chubby!", "I gotta work work work work work~", "Do you have free time? Yes? Good for you!", "It's time for you to eat something! I suggest a healthy snack?", "It's afternoon now, in case you can't read the clock."];
+		return afternoonChoices[Math.floor(Math.random()*afternoonChoices.length)];
 	}
 	
 	function eveningGreet() {
-		
+		var eveningChoices = ["Good evening!", "Well, how was diner? Or do you always eat before six?", "Look at the sunset! It only comes once a day.", "Relax and sit back, I think you'll need that!", "Go watch a movie, the evening is the perfect time for it.", "Watch out if you go out, I don't want you to get hurt!", "Do you like the evening? I do, because that's when you can see the sunset.", "Again, it's evening now. Aren't you supposed to be preparing to go to bed?"];
+		return eveningChoices[Math.floor(Math.random()*eveningChoices.length)];
 	}
 	
 	function nightGreet() {
-		
+		var nightChoices = ["Good night!", "You're still going out this late? Well, be safe!", "WHERE'S THE PARTY? OUT THERE IS THE PARTY!", "Zzzz, go sleep. But if you must go out, allow me to give you advice.", "Watch out for thieves at this hour, just be safe okay?", "Oh noes, at this hour vampires like to go out and suck your blood!", "Hmm, the night. It's so calm and beautiful, I can relate to why you want to go out now!", "Heeeeeeeey, be safe okay?"];
+		return nightChoices[Math.floor(Math.random()*nightChoices.length)];
 	}	
 }
 
